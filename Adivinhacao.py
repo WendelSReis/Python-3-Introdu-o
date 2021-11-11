e9770a75dc13bf2b8e0444ec7d474db89469e612
WendelSReis <90618369+WendelSReis@users.noreply.github.com>
2021-11-10 22:01:08 -0300

    Update Adivinhacao.py

--- Adivinhacao.py
+++ Adivinhacao.py
@@ -2,7 +2,7 @@ print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
+numero_secreto = 7
 
 chute_str = input("Digite o seu número: ")
 print("Você digitou: ", chute_str)
